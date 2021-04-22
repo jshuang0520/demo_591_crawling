7b1c1fa4f137aa197bf3ff60a5aaae9ffdaec96d
jshuang0520 <s9140911@yahoo.com.tw>
2021-04-23 01:01:07 +0800

    chore: verify the number of data

--- src/try_functions/try_main_crawling.py
+++ src/try_functions/try_main_crawling.py
@@ -20,22 +20,31 @@ global_config = set_env(logger=global_logger,
 mongodb_client = MongodbUtility(global_config, global_logger)
 db_conn = mongodb_client.db_connect(database='test')  # mongodb_client.dflt_conn_db
 
-# mongodb_client.delete(conn_db=db_conn, coll_name='my_collection', delete_criteria={})
+# # mongodb_client.delete(conn_db=db_conn, coll_name='my_collection', delete_criteria={})
+#
+#
+# # taipei_data = main(city='taipei_city')
+# # # print('type(taipei_data), len(taipei_data):', type(taipei_data), len(taipei_data), taipei_data[0:10], type(taipei_data[0]))
+# # mongodb_client.create_collection(conn_db=db_conn, coll_name='taipei_city_renting')
+# # mongodb_client.create_index(conn_db=db_conn, coll_name='taipei_city_renting', idx_col_list=['post_id', 'gender_request', 'city', 'phone', 'owner_identity', 'owner_last_name', 'owner_gender'])
+# # # mongodb_client.create(conn_db=db_conn, coll_name='my_collection', data_to_insert=taipei_data)  # taipei_data is a list of dict
+# # mongodb_client.update(conn_db=db_conn, coll_name='taipei_city_renting', data_to_insert=taipei_data, unique_key='post_id')
+# mongodb_client.read(conn_db=db_conn, coll_name='taipei_city_renting', query_criteria=None, projection=None)
+#
+#
+# # new_taipei_data = main(city='new_taipei_city')
+# # # print('type(new_taipei_data), len(new_taipei_data):', type(new_taipei_data), len(new_taipei_data), new_taipei_data[0:10], type(new_taipei_data[0]))
+# # mongodb_client.create_collection(conn_db=db_conn, coll_name='new_taipei_city_renting')
+# # mongodb_client.create_index(conn_db=db_conn, coll_name='new_taipei_city_renting', idx_col_list=['post_id', 'gender_request', 'city', 'phone', 'owner_identity', 'owner_last_name', 'owner_gender'])
+# # # mongodb_client.create(conn_db=db_conn, coll_name='my_collection', data_to_insert=new_taipei_data)  # taipei_data is a list of dict
+# # mongodb_client.update(conn_db=db_conn, coll_name='new_taipei_city_renting', data_to_insert=new_taipei_data, unique_key='post_id')
+# mongodb_client.read(conn_db=db_conn, coll_name='new_taipei_city_renting', query_criteria=None, projection=None)
 
 
-# taipei_data = main(city='taipei_city')
-# # print('type(taipei_data), len(taipei_data):', type(taipei_data), len(taipei_data), taipei_data[0:10], type(taipei_data[0]))
-# mongodb_client.create_collection(conn_db=db_conn, coll_name='taipei_city_renting')
-# mongodb_client.create_index(conn_db=db_conn, coll_name='taipei_city_renting', idx_col_list=['post_id', 'gender_request', 'city', 'phone', 'owner_identity', 'owner_last_name', 'owner_gender'])
-# # mongodb_client.create(conn_db=db_conn, coll_name='my_collection', data_to_insert=taipei_data)  # taipei_data is a list of dict
-# mongodb_client.update(conn_db=db_conn, coll_name='taipei_city_renting', data_to_insert=taipei_data, unique_key='post_id')
-mongodb_client.read(conn_db=db_conn, coll_name='taipei_city_renting', query_criteria=None, projection=None)
-
-
-# new_taipei_data = main(city='new_taipei_city')
-# # print('type(new_taipei_data), len(new_taipei_data):', type(new_taipei_data), len(new_taipei_data), new_taipei_data[0:10], type(new_taipei_data[0]))
-# mongodb_client.create_collection(conn_db=db_conn, coll_name='new_taipei_city_renting')
-# mongodb_client.create_index(conn_db=db_conn, coll_name='new_taipei_city_renting', idx_col_list=['post_id', 'gender_request', 'city', 'phone', 'owner_identity', 'owner_last_name', 'owner_gender'])
-# # mongodb_client.create(conn_db=db_conn, coll_name='my_collection', data_to_insert=new_taipei_data)  # taipei_data is a list of dict
-# mongodb_client.update(conn_db=db_conn, coll_name='new_taipei_city_renting', data_to_insert=new_taipei_data, unique_key='post_id')
-mongodb_client.read(conn_db=db_conn, coll_name='new_taipei_city_renting', query_criteria=None, projection=None)
+from bson.json_util import dumps
+res = db_conn.taipei_city_renting.find({'post_id': 10700982})
+print([eval(dumps(r)) for r in res])
+doc_count = db_conn.taipei_city_renting.count_documents({})
+print(doc_count)
+doc_count = db_conn.new_taipei_city_renting.count_documents({})
+print(doc_count)
