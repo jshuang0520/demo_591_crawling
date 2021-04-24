3d5f01e683f8cf264ed0b957d40087b4ba0f1f4b
jshuang0520 <s9140911@yahoo.com.tw>
2021-04-25 01:09:36 +0800

    feat: update swagger doc (api document)

--- app/mongo_api.py
+++ app/mongo_api.py
@@ -13,7 +13,7 @@ from src.database.mongo.mongodb import MongodbUtility, ApiQuery
 global_logger = Logger().get_logger('api')
 dir_path = os.path.dirname(os.path.realpath(__file__))
 global_config = set_env(logger=global_logger,
-                        env_file_path=str(dir_path.split('app')[0])+'env_files/dev/.env',
+                        env_file_path=str(dir_path.split('app')[0]) + 'env_files/dev/.env',
                         config_folder_name='configs')
 
 app = Flask(__name__)
@@ -76,16 +76,199 @@ health_payload_request = api.model('HealthPayload',  # Model name in $ref we see
                                                              example="1"),
                                         limit=fields.String(required=True,
                                                             description='limit',
-                                                            example="100"))
-                                   )
+                                                            example="100")))
 # response
 health_response = api.model('HealthResponse',
                             dict(response=fields.Integer(required=False,
                                                          description='response code',
-                                                         example=200)
-                                 )
+                                                         example=200)))
 
-                            )
+#######################################################################################################################
+
+# common fields
+city_enum_fields = fields.String(required=True,
+                                 description='city',
+                                 enum=['taipei_city', 'new_taipei_city'])
+
+gender_enum_fields = fields.String(required=True,
+                                   description='gender',
+                                   enum=['男', '女'])
+
+owner_identity_fields = fields.String(required=True,
+                                      description='owner\'s identity',
+                                      example='屋主 or 仲介 or ...')
+
+mongo_object_id_fields = fields.String(required=True,
+                                       description='ObjectId in mongodb',
+                                       example='6080c4693d3a69615bf56100')
+
+floor_fields = fields.String(required=True,
+                             description='floor',
+                             example='3F')
+
+story_fields = fields.String(required=True,
+                             description='floor and number of storie',
+                             example='3F/3F')
+
+gender_request_fields = fields.String(required=True,
+                                      description='gender request to renters',
+                                      example='男女生皆可')
+
+lot_size_fields = fields.String(required=True,
+                                description='lot size; area',
+                                example='6坪')
+
+nick_name_fields = fields.String(required=True,
+                                 description='information of contactor',
+                                 example='仲介 沈先生')
+
+owner_last_name_fields = fields.String(required=True,
+                                       description='last name',
+                                       example='吳 or 黃 or 王 or 潘 or 楊 or 李 or 林 or 張')
+
+phone_fields = fields.String(required=True,
+                             description='phone number',
+                             example='0912-345-678')
+
+post_id_fields = fields.Integer(required=True,
+                                description='post_id on 591 webpage, for each apartment object',
+                                example=9890771)
+
+renter_fields = fields.String(required=True,
+                              description='renter',
+                              example='沈先生')
+
+housing_object_status_fields = fields.String(required=True,
+                                             description='status of the housing object',
+                                             example='獨立套房')
+
+housing_object_types_fields = fields.String(required=True,
+                                            description='types of the housing object',
+                                            example='公寓')
+
+"""
+{
+  "code": 0, 
+  "data": [
+    {
+      "_id": "6080c4693d3a69615bf56100", 
+      "city": "台北市", 
+      "floor": "3F", 
+      "gender_request": "男女生皆可", 
+      "lot_size": "6坪", 
+      "nick_name": "仲介 沈先生", 
+      "owner_gender": "男", 
+      "owner_identity": "仲介", 
+      "owner_last_name": "沈", 
+      "phone": "0905-059-091", 
+      "post_id": 9890771, 
+      "renter": "沈先生", 
+      "status": "獨立套房", 
+      "story": "3F/3F", 
+      "types": "公寓"
+    }, 
+  ], 
+  "message": "success", 
+  "time_elapsed_api": 0.175548
+
+"""
+# response - api common return
+resp_data_dict = api.model('ColumnDataInResponse',
+                           dict(_id=mongo_object_id_fields,
+                                city=city_enum_fields,
+                                floor=floor_fields,
+                                gender_request=gender_request_fields,
+                                lot_size=lot_size_fields,
+                                nick_name=nick_name_fields,
+                                owner_gender=gender_enum_fields,
+                                owner_identity=owner_identity_fields,
+                                owner_last_name=owner_last_name_fields,
+                                phone=phone_fields,
+                                post_id=post_id_fields,
+                                renter=renter_fields,
+                                status=housing_object_status_fields,
+                                story=story_fields,
+                                types=housing_object_types_fields))
+
+response_format = api.model('CommonResponseFormat',
+                            dict(code=fields.Integer(required=False,
+                                                     description='response code',
+                                                     example=0),
+                                 data=fields.List(fields.Nested(model=resp_data_dict)),
+                                 message=fields.String(required=False,
+                                                       description='response code',
+                                                       enum=['success', 'failed']),
+                                 time_elapsed_api=fields.Float(required=False,
+                                                               description='elapsed time from api return',
+                                                               example=0.001)))
+
+#######################################################################################################################
+
+"""
+
+curl \
+-H "Content-Type: application/json" \
+-d '{"city": "taipei_city", "gender":"男"}' \
+-X POST \
+http://127.0.0.1:30000/api/v1/renter/gender
+
+"""
+
+# request payload
+renter_gender_req_payload = api.model('RenterGenderRequestPayload',  # Model name in $ref we see
+                                      dict(city=city_enum_fields,
+                                           gender=gender_enum_fields))
+
+#######################################################################################################################
+
+"""
+
+curl \
+-H "Content-Type: application/json" \
+-d '{"phone": "0905-059-091"}' \
+-X POST \
+http://127.0.0.1:30000/api/v1/owner/phone
+
+"""
+
+# request payload
+owner_phone_req_payload = api.model('OwnerPhoneRequestPayload',  # Model name in $ref we see
+                                    dict(phone=phone_fields))
+
+#######################################################################################################################
+
+"""
+
+curl \
+-H "Content-Type: application/json" \
+-d '{"negative_id_lst": ["屋主"]}' \
+-X POST \
+http://127.0.0.1:30000/api/v1/owner/identity
+
+"""
+
+# request payload
+owner_identity_req_payload = api.model('OwnerIdentityRequestPayload',  # Model name in $ref we see
+                                       dict(negative_id_lst=fields.List(owner_identity_fields)))
+
+#######################################################################################################################
+
+"""
+
+curl \
+-H "Content-Type: application/json" \
+-d '{"city": "new_taipei_city", "owner_gender": "男", "owner_last_name": "楊"}' \
+-X POST \
+http://127.0.0.1:30000/api/v1/owner/gender/last-name
+
+"""
+
+# request payload
+owner_gender_lastname_req_payload = api.model('OwnerGenderLastNameRequestPayload',  # Model name in $ref we see
+                                              dict(city=city_enum_fields,
+                                                   owner_gender=gender_enum_fields,
+                                                   owner_last_name=owner_last_name_fields))
+
 
 #######################################################################################################################
 
@@ -111,8 +294,9 @@ def healthcheck():
 
 @app.route('/version/', strict_slashes=False)
 def hello_world():
-    message = dict(version_info='591 House Renting - API Version:{version}, :{tag}\n'.format(version=version, tag=repo_tag),
-                   document_info='Please refer to: ' + server_url + '/swagger/')
+    message = dict(
+        version_info='591 House Renting - API Version:{version}, :{tag}\n'.format(version=version, tag=repo_tag),
+        document_info='Please refer to: ' + server_url + '/swagger/')
     return message
 
 
@@ -129,7 +313,8 @@ class RenterGender(Resource):
         self.config = set_env(logger=self.logger, env_file_path=env_file, config_folder_name='configs')
         self.query_handler = ApiQuery(global_config, global_logger)
 
-    @ns_renter.response(200, 'Success')
+    @ns_renter.doc(body=renter_gender_req_payload)  # request payload schema
+    @ns_renter.response(200, 'Success', response_format)
     def post(self):
         """
         given city of house and renter's gender request, return apartments
@@ -188,7 +373,8 @@ class OwnerPhone(Resource):
         self.config = set_env(logger=self.logger, env_file_path=env_file, config_folder_name='configs')
         self.query_handler = ApiQuery(global_config, global_logger)
 
-    @ns_owner.response(200, 'Success')
+    @ns_owner.doc(body=owner_phone_req_payload)  # request payload schema
+    @ns_owner.response(200, 'Success', response_format)
     def post(self):
         """
         given owner's phone number, return apartments
@@ -244,7 +430,8 @@ class OwnerIdentity(Resource):
         self.config = set_env(logger=self.logger, env_file_path=env_file, config_folder_name='configs')
         self.query_handler = ApiQuery(global_config, global_logger)
 
-    @ns_owner.response(200, 'Success')
+    @ns_owner.doc(body=owner_identity_req_payload)  # request payload schema
+    @ns_owner.response(200, 'Success', response_format)
     def post(self):
         """
         given owner's identity (using positive or negative listing), return apartments
@@ -300,7 +487,8 @@ class OwnerGenderLastname(Resource):
         self.config = set_env(logger=self.logger, env_file_path=env_file, config_folder_name='configs')
         self.query_handler = ApiQuery(global_config, global_logger)
 
-    @ns_owner.response(200, 'Success')
+    @ns_owner.doc(body=owner_gender_lastname_req_payload)  # request payload schema
+    @ns_owner.response(200, 'Success', response_format)
     def post(self):
         """
         given city, owner's gender and last name, return apartments
@@ -313,7 +501,8 @@ class OwnerGenderLastname(Resource):
 
         if necessary_keys.issubset(payload.keys()):
             self.logger.info("payload: {payload_type}, {payload}".format(payload_type=type(payload), payload=payload))
-            res = self.query_handler.query_owner_gender_last_name(payload['city'], payload['owner_gender'], payload['owner_last_name'])
+            res = self.query_handler.query_owner_gender_last_name(payload['city'], payload['owner_gender'],
+                                                                  payload['owner_last_name'])
             # res = self.query_handler.query_owner_gender_last_name('new_taipei_city', '男', '楊')  # FIXME: test
             end = time.time()
             time_elapsed_api = float("{:.6f}".format(end - start))
@@ -347,7 +536,7 @@ class OwnerGenderLastname(Resource):
 
 
 if __name__ == '__main__':
-    server_port = '30000'  # FIXME: it should be port 30000
+    server_port = '30000'  # FIXME: it should be in config settings
 
     api.add_namespace(ns_renter)
     api.add_namespace(ns_owner)
@@ -356,7 +545,6 @@ if __name__ == '__main__':
 
 """
 Sample request payload
-
 
 ===
 
